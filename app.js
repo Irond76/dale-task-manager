5ac04730ccee54eae5bef7dacd8878461eb6b1a3
Dale Bendixen <dalebendixen@gmail.com>
2022-08-09 16:54:55 -0600

    set-up connections

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config()
 const port = 3000;
 
 // MIDDLEWARE
+
 app.use(express.json());
 
 // ROUTES
@@ -13,12 +17,17 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks);
 
-// app.get('/api/v1/tasks')   -get all the tasks
-// app.post('/api/v1/tasks')     -create a new task
-// app.get('/api/v1/tasks/:id')   -get single task
-// app.patch('/api/v1/tasks/:id')    - update task 
-// app.delete('/api/v1/tasks/:id')    - delete task 
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server Listening On Port ${port}...`));
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start();
 
 
 
-app.listen(port, console.log(`Server Listening On Port ${port}...`));
+
